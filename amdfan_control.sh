ae41cb1cfa4e1a097748e7a31eb986c5170b8218
Monzilur Rahman <monzilur.rahman@gmail.com>
2022-01-02 16:15:17 -0500

    Update amdfan_control.sh

--- amdfan_control.sh
+++ amdfan_control.sh
@@ -4,6 +4,3 @@ echo 150 | sudo tee -a /sys/class/drm/card0/device/hwmon/hwmon0/pwm1
 
 echo 1 | sudo tee -a /sys/class/drm/card1/device/hwmon/hwmon1/pwm1_enable
 echo 150 | sudo tee -a /sys/class/drm/card1/device/hwmon/hwmon1/pwm1
-
-# refresh rate
-rxboost -d 2 -i 30 -d 3 -i 30
