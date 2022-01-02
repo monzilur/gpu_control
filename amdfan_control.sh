a0ffb9c96504fce36c9ef29a0d4460927e424732
Monzilur Rahman <monzilur.rahman@gmail.com>
2022-01-02 16:41:39 -0500

    Update amdfan_control.sh

--- amdfan_control.sh
+++ amdfan_control.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # for 58% fan speed
-sudo echo 1 > /sys/class/drm/card0/device/hwmon/hwmon0/pwm1_enable && sudo echo 150 > /sys/class/drm/card0/device/hwmon/hwmon0/pwm1
+sudo echo 1 > /sys/class/drm/card0/device/hwmon/hwmon0/pwm1_enable
+sudo echo 150 > /sys/class/drm/card0/device/hwmon/hwmon0/pwm1
 
-sudo echo 1 > /sys/class/drm/card1/device/hwmon/hwmon1/pwm1_enable && sudo echo 150 > /sys/class/drm/card1/device/hwmon/hwmon1/pwm1
+sudo echo 1 > /sys/class/drm/card1/device/hwmon/hwmon1/pwm1_enable
+sudo echo 150 > /sys/class/drm/card1/device/hwmon/hwmon1/pwm1
