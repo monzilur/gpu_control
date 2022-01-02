d3858801b6168197d12f3ab24cdefdcdc0f41761
Monzilur Rahman <monzilur.rahman@gmail.com>
2022-01-02 16:01:20 -0500

    Update amdfan_control.sh

--- amdfan_control.sh
+++ amdfan_control.sh
@@ -1,8 +1,3 @@
-curl -k https://example.com/script.sh > /home/miner/userstartupscript.sh && sh /home/miner/userstartupscript.sh
-	
-#while sleep 1; do echo 0 > /sys/class/drm/card0/device/hwmon/hwmon1/pwm1_enable; done
-rxboost -d 2 -i 30 -d 3 -i 30
-
 # for 58% fan speed
 echo 1 | sudo tee -a /sys/class/drm/card0/device/hwmon/hwmon0/pwm1_enable
 echo 150 | sudo tee -a /sys/class/drm/card0/device/hwmon/hwmon0/pwm1
